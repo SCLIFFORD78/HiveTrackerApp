364a7ba17970f8cd1e3a0f7b5971f7d9a3931759
SCLIFFORD78 <faugh01@gmail.com>
2021-10-25 00:34:45 +0100

    nav bar half working

--- app/src/main/java/org/wit/hivetrackerapp/activities/Home.kt
+++ app/src/main/java/org/wit/hivetrackerapp/activities/Home.kt
@@ -2,11 +2,41 @@ package org.wit.hivetrackerapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
 import org.wit.hivetrackerapp.R
+import org.wit.hivetrackerapp.databinding.HomeBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding : HomeBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.addFragment, R.id.listFragment), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
 }
