a9b6ac5a0ac212348df9d32b3577317daf656789
SCLIFFORD78 <faugh01@gmail.com>
2021-10-25 12:43:05 +0100

    Image selector working in fragments

--- app/src/main/java/org/wit/hivetrackerapp/fragments/AddFragment.kt
+++ app/src/main/java/org/wit/hivetrackerapp/fragments/AddFragment.kt
@@ -1,5 +1,7 @@
 package org.wit.hivetrackerapp.fragments
 
+import android.app.Activity.RESULT_CANCELED
+import android.app.appsearch.AppSearchResult.RESULT_OK
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -15,7 +17,11 @@ import org.wit.hivetrackerapp.databinding.FragmentAddBinding
 import org.wit.hivetrackerapp.main.MainApp
 import org.wit.hivetrackerapp.models.HiveModel
 import timber.log.Timber.i
-
+import org.wit.hivetrackerapp.helpers.showImagePicker
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.squareup.picasso.Picasso
 
 
 class AddFragment : Fragment() {
@@ -24,6 +30,8 @@ class AddFragment : Fragment() {
     private val fragBinding get() = _fragBinding!!
     val hive = HiveModel()
     var edit = false
+    private lateinit var imageIntentLauncher: ActivityResultLauncher<Intent>
+
 
 
 
@@ -44,6 +52,8 @@ class AddFragment : Fragment() {
         val root = fragBinding.root
         activity?.title = getString(R.string.action_add)
         setAddButtonListener(fragBinding)
+        setChooseImageListener(fragBinding)
+        registerImagePickerCallback(fragBinding)
         return root;
     }
 
@@ -78,5 +88,34 @@ class AddFragment : Fragment() {
             //setResult(RESULT_OK)
             Navigation.findNavController(this.requireView()).navigate(R.id.listFragment)
         }
+    }
+
+    fun setChooseImageListener(layout: FragmentAddBinding){
+        layout.chooseImage.setOnClickListener{
+            showImagePicker(imageIntentLauncher)
+        }
+    }
+
+    private fun registerImagePickerCallback(layout: FragmentAddBinding) {
+        imageIntentLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                when (result.resultCode) {
+                    -1 -> {
+                        if (result.data != null) {
+                            i("Got Result ${result.data!!.data}")
+                            hive.image = result.data!!.data!!
+                            Picasso.get()
+                                .load(hive.image)
+                                .into(layout.hiveImage)
+                        } // end of if
+                    }
+                    0 -> {
+                    }
+                    else -> {
+                        i("Image selection cancelled")
+                    }
+                }
+            }
     }
 }
--- app/src/main/java/org/wit/hivetrackerapp/main/MainApp.kt
+++ app/src/main/java/org/wit/hivetrackerapp/main/MainApp.kt
@@ -15,8 +15,8 @@ class MainApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        //hives = HiveJSONStore(applicationContext)
-        hives = HiveMemStore()
+        hives = HiveJSONStore(applicationContext)
+        //hives = HiveMemStore()
         i("HiveTracker started")
         //hives.add(HiveModel("One", "About one..."))
         //hives.add(HiveModel("Two", "About two..."))
