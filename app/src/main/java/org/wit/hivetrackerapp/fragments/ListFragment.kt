fcc3d625da5819861f8bf91ea3b93aae27aee5e6
SCLIFFORD78 <faugh01@gmail.com>
2021-10-28 11:03:54 +0100

    Initial login pages added

--- app/src/main/java/org/wit/hivetrackerapp/fragments/ListFragment.kt
+++ app/src/main/java/org/wit/hivetrackerapp/fragments/ListFragment.kt
@@ -19,11 +19,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.hivetrackerapp.R
 import org.wit.hivetrackerapp.activities.Home
 import org.wit.hivetrackerapp.adapters.HiveTrackerAdapter
+import org.wit.hivetrackerapp.data.model.LoggedInUser
 import org.wit.hivetrackerapp.databinding.FragmentAddBinding
 import org.wit.hivetrackerapp.databinding.FragmentListBinding
 import org.wit.hivetrackerapp.databinding.HomeBinding
 import org.wit.hivetrackerapp.main.MainApp
 import org.wit.hivetrackerapp.models.HiveModel
+import org.wit.hivetrackerapp.ui.login.LoggedInUserView
 import timber.log.Timber
 
 class ListFragment : Fragment(), HiveTrackerAdapter.OnHiveClickListener {
